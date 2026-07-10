33ba1f0586ae3b8707b02879fe07d06de772deb4
Kaiden-Bell <kaidenbell@unr.edu>
2026-07-10 15:58:26 -0700

    Fix: Fixed pdflatex not appearing error, moved logs into folder, and seperated pdfs

--- compiler.py
+++ compiler.py
@@ -7,13 +7,27 @@ Description: Complies LaTex to PDF using Python processes.
 
 import subprocess
 import os
+import shutil
 
+def compile_latex_to_pdf(file, log_dir="output/logs/LaTeX_logs", pdf_dir="output/pdfs"):
+    """Compile LaTeX file to PDF using pdflatex"""
 
-def compile_latex_to_pdf(file):
-    """Compile LaTeX file to PDF using pdflatex command."""
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(pdf_dir, exist_ok=True)
+
     try:
-        subprocess.run(['pdflatex', '-interaction=nonstopmode', file], 
+        subprocess.run(['/usr/bin/pdflatex', '-interaction=nonstopmode',f'-output-directory={log_dir}', file], 
                       check=True, capture_output=True)
+        
+        base_name = os.path.splitext(os.path.basename(file))[0]
+        generated_pdf = os.path.join(log_dir, f"{base_name}.pdf")
+        target_pdf = os.path.join(pdf_dir, f"{base_name}.pdf")
+
+        if os.path.exists(generated_pdf):
+            shutil.move(generated_pdf, target_pdf)
+            print("Success!")
+        else:
+            print("Compilation Finished, unable to move file")
     except subprocess.CalledProcessError as e:
         print(f"Error compiling LaTeX: {e}")
 
